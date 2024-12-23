4b0f7b89eba30cc12789cf6a4d1148bce0d51890
GMKrishna99 <gmohankrishna23@gmail.com>
2024-12-23 15:21:07 +0530

    button and button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { FaChevronRight } from "react-icons/fa";
 
 // Reusable Button Component
-const Button = ({ children, onClick, style, className }) => {
+const Button = ({ children, onClick }) => {
   return (
-    <button onClick={onClick} style={style} className={className}>
+    <button onClick={onClick} className="btn_outline">
       {children}
+      <div className="btn_outline_circle ">
+        <FaChevronRight className="w-4 h-4" />
+      </div>
     </button>
   );
 };
